d816c2e1d1cdf01410cc7e59aeba7b0dc0023876
Sam <29834568+esvanha@users.noreply.github.com>
2024-04-10 23:31:51 +0200

    Initial commit

---
+++ back_end_api/Program.cs
@@ -0,0 +1,80 @@
+using System.Text;
+using RabbitMQ.Client;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var factory = new ConnectionFactory {
+    HostName = "rabbitmq",
+    UserName = Environment.GetEnvironmentVariable("RABBITMQ_USERNAME"),
+    Password = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD")
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+const string message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Use(async (context, next) =>
+{
+    // Log the requested path
+    Console.WriteLine($"Requested Path: {context.Request.Path}");
+    
+    await next.Invoke();
+});
+
+//app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/WeatherForecast", () =>
+{
+    channel.BasicPublish(exchange: string.Empty,
+                         routingKey: "hello",
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine($" [x] Sent {message}");
+
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ front_end/src/App.jsx
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+//    let ignore = false;
+
+    fetch('/api/WeatherForecast').then(response => {
+      if (!response.ok) {
+        throw new Error('Non-OK HTTP status');
+      }
+      return response.json();
+    }).then(json => {
+      console.log(json);
+ //     if (!ignore) {
+        setIsLoading(false);
+        setResults(json)
+  //    }
+    }).catch(err => setError(err));
+
+   // return () => {
+   //   ignore = true;
+   // }
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  } else if (error != null) {
+    return <p>Error: {error}</p>
+  } else {
+    return (
+      <ul>
+        {results.map((result, index) => (
+          <li key={index}>
+            {result['date']} &mdash; {result['temperatureC']} &#x2103; &mdash; {result['summary']}
+          </li>
+        ))}
+      </ul>
+    )
+  }
+}
+
+export default App;
