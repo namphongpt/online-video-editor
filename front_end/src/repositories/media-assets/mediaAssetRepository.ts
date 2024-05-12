4be773e7f2f131ee0acd4ade299608fa118bdc8b
Sam <29834568+esvanha@users.noreply.github.com>
2024-05-13 01:04:09 +0200

    Fix path trailing / in mediaAssetRepository.ts

--- front_end/src/repositories/media-assets/mediaAssetRepository.ts
+++ front_end/src/repositories/media-assets/mediaAssetRepository.ts
@@ -5,12 +5,12 @@ import { MediaAsset } from '@/interfaces/mediaAsset';
 type MediaAssets = ReadonlyArray<MediaAsset>;
 
 export const fetchMediaAssets = (): Promise<MediaAssets> =>
-    axios.get(`/MediaAssets`).then(res => res.data);
+    axios.get(`/MediaAssets/`).then(res => res.data);
 
 export const createMediaAsset = (mediaAssetParam: CreateMediaAssetParam): Promise<MediaAsset> => {
     const formData = new FormData();
     formData.append('mediaFile', mediaAssetParam.mediaFile);
 
-    return axios.postForm(`/MediaAssets`, formData)
+    return axios.postForm(`/MediaAssets/`, formData)
                 .then(res => res.data);
 }
